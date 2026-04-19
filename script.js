9c8459e19385ec97c5a32cfb96e62e0e40b9d768
Amber Wang <wangshengyan6@gmail.com>
2026-04-19 16:00:02 +0800

    Fix: API link

--- script.js
+++ script.js
@@ -2,36 +2,40 @@ const quote = document.querySelector(".quote");
 const author = document.querySelector(".author");
 const newQuote = document.querySelector(".new-quote-btn");
 const share = document.querySelector(".share-btn");
-const URL = "https://api.quotable.io/quotes/random";
+const URL = "http://api.quotable.io/quotes/random";
+
 async function getQuote(url) {
-  console.log("click");
+
   try {
+
     const response = await fetch(url);
     if (response.status === 429) {
-      alert("Please try again later!!");
-      return;
+      throw new Error("Too many requests.")
     }
+
     if (!response.ok) {
-      alert("Failed to fetch quote.");
-      return;
+      throw new Error(`HTTP status: ${response.status}`)
     }
+
     const data = await response.json();
-    console.log(data);
-    quote.innerHTML = data.content;
-    author.innerHTML = data.author;
+    quote.innerHTML = `" ${data[0].content} "`;
+    author.innerHTML = data[0].author;
+
   } catch (error) {
-    alert("An error occurred while fetching the quote.");
-    console.error(error);
+    alert(error.message);
   }
 }
+
 function shareQuote() {
   window.open(
     `https://twitter.com/intent/tweet?text=${quote.innerHTML}---${author.innerHTML}`
   );
 }
+
 newQuote.addEventListener("click", () => {
   getQuote(URL);
 });
+
 share.addEventListener("click", () => {
   shareQuote();
 });
